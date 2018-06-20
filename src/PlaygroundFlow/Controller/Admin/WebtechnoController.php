0c8efbc7e6bd87127b77a9be9d0e641fde0f8520
Greg Besson <bessong@gmail.com>
2018-06-20 16:16:58 +0200

    Update WebtechnoController.php

--- src/PlaygroundFlow/Controller/Admin/WebtechnoController.php
+++ src/PlaygroundFlow/Controller/Admin/WebtechnoController.php
@@ -9,7 +9,7 @@ use PlaygroundFlow\Entity\OpenGraphStoryMapping as Mapping;
 use PlaygroundFlow\Entity\OpenGraphWebTechno as WebTechno;
 use PlaygroundFlow\Entity\OpenGraphObjectMapping as ObjectMapping;
 
-class WebtechnoController extends AbstractActionController
+class WebTechnoController extends AbstractActionController
 {
     /**
      * @var WebTechnoService
